7ad9cd5adb976bbb34dae67d6ebf53e4bbd5cefc
eylemugurel <eylemugurel@hotmail.com>
2024-10-21 23:32:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# TemplateLibraryTest
+
+[![test](https://github.com/DaphneWebFramework/TemplateLibraryTest/actions/workflows/test.yml/badge.svg)](https://github.com/DaphneWebFramework/TemplateLibraryTest/actions/workflows/test.yml)
+
+This repository contains tests for the [TemplateLibrary](https://github.com/DaphneWebFramework/TemplateLibrary) library.
+
+## License
+
+This project is distributed under the Creative Commons Attribution 4.0 International License. For more information, visit [CC BY 4.0 License](https://creativecommons.org/licenses/by/4.0/).
---
+++ test/bootstrap.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+/**
+ * bootstrap.php
+ *
+ * (C) 2024 by Eylem Ugurel
+ *
+ * Licensed under a Creative Commons Attribution 4.0 International License.
+ *
+ * You should have received a copy of the license along with this work. If not,
+ * see <http://creativecommons.org/licenses/by/4.0/>.
+ */
+
+\spl_autoload_register(function(string $className): void {
+    $classFilePath = \rtrim(__DIR__, '/\\')
+                   . '/../source/classes/'
+                   . \str_replace('\\', '/', $className)
+                   . '.php';
+    if (!\is_file($classFilePath)) {
+        return;
+    }
+    require $classFilePath;
+});
